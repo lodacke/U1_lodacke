9c577f18d49014f1d6cf85b046ea716f9807a07c
lodacke <lo.dacke@mail.com>
2023-02-15 15:03:05 +0100

    Update quiz.js

--- problem/js/quiz.js
+++ problem/js/quiz.js
@@ -7,7 +7,7 @@ function get_game(){
     </div>
     <div id=game></div>`;
 
-        
+        document.querySelector("body").style.backgroundColor = "rgb(191, 232, 237);";
         document.querySelector("#feedback").textContent = "Getting a random image..."
         document.querySelector("main").style.backgroundImage = "url('media/logo.png')";
     
