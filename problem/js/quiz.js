8cf7d57cb7eb2d9955ec5e75aa9a9d43634a33e7
lodacke <lo.dacke@mail.com>
2023-02-15 15:14:21 +0100

    Added start to game

--- problem/js/quiz.js
+++ problem/js/quiz.js
@@ -1,4 +1,8 @@
 
+function random_number(max) {
+    return Math.floor(max * Math.random());
+}
+
 function get_game(){
     document.querySelector("main").innerHTML = `
     <div id=login_box>
@@ -11,6 +15,25 @@ function get_game(){
         document.querySelector("#feedback").textContent = "Getting a random image..."
         document.querySelector("main").style.backgroundImage = "url('media/logo.png')";
     
+
+        start_game()
+    }
+
+    function start_game(){
+        let game_board = document.createElement("div");
+        document.querySelector("main").append(game_board);
+
+        let game_photo = document.createElement("div");
+        let question_board = document.createElement("div");
+        game_board.append(question_board);
+        game_board.append(game_photo);
+
+        for(let i = 0; i < 4; i++){
+            let question_dom = document.createElement("div");
+            question_board.append(question_dom);
+            question_dom.textContent = random_number(ALL_BREEDS.length);
+
+        }
     }
     
     
